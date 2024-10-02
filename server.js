002684e7e96df01dd754b126ea90dcab1bb42278
paolasoria <paola.soria0597@gmail.com>
2024-10-02 10:28:49 -0400

    add middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const recipeController = require('./controllers/recipes.js')
@@ -35,7 +37,10 @@ app.get('/', (req, res) => {
     user: req.session.user,
   });
 });
+
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
 app.use('/recipes', recipeController);
 app.use('/ingredients', ingredientController);
 
