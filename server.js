06ccc5f8533a300c67131bd8c3fc7c4acdd29b6d
paolasoria <paola.soria0597@gmail.com>
2024-10-05 11:00:02 -0400

    updated routes and views folder

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
-const isSignedIn = require('./middleware/is-signed-in.js');
-const passUserToView = require('./middleware/pass-user-to-view.js');
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require("express-session");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
-const authController = require('./controllers/auth.js');
-const recipeController = require('./controllers/recipes.js')
-const ingredientController = require('./controllers/ingredients.js')
+const authController = require("./controllers/auth.js");
+const recipesController = require("./controllers/recipes.js");
+const ingredientsController = require("./controllers/ingredients.js");
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT ? process.env.PORT : "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-app.use(morgan('dev'));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -31,29 +27,21 @@ app.use(
     saveUninitialized: true,
   })
 );
-
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
-
 app.use(passUserToView);
-app.use('/auth', authController);
-app.use(isSignedIn);
-app.use('/recipes', recipeController);
-app.use('/ingredients', ingredientController);
 
-
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
+app.use("/auth", authController);
+app.use(isSignedIn);
+app.use("/recipes", recipesController);
+app.use("/ingredients", ingredientsController);
 
-app.listen(3000, () => {
-  console.log(`The express app is ready on port ${port}!`);
+mongoose.connection.on("connected", () => {
+  console.clear();
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+  });
 });
